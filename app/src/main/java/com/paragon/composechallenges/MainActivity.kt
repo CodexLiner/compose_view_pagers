dfabb56fd5cd27523e6893ebfe48455642b07362
Gopal Meena <meenagopal24@gmail.com>
2024-09-04 14:55:03 +0530

    navigation and home screen

--- app/src/main/java/com/paragon/composechallenges/MainActivity.kt
+++ app/src/main/java/com/paragon/composechallenges/MainActivity.kt
@@ -8,23 +8,59 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.paragon.composechallenges.ui.home.HomeScreen
 import com.paragon.composechallenges.ui.theme.ComposeChallengesTheme
+import com.paragon.composechallenges.ui.viewPager.CarouselImplementationWithTabIndicators
+import com.paragon.composechallenges.ui.viewPager.DynamicallyAddRemovePagesInViewPager
 import com.paragon.composechallenges.ui.viewPager.NestingOfViewPagerAndControllingWithParent
 
+sealed class NavigationItem(var route: String) {
+    data object HomeScreen : NavigationItem("HomeScreen")
+    data object NestedViewPager : NavigationItem("NestedViewPager")
+    data object DynamicViewPager : NavigationItem("DynamicViewPager")
+    data object CarouselViewPager : NavigationItem("CarouselViewPager")
+}
+
+
 val EMPTY = ""
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             ComposeChallengesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        NestingOfViewPagerAndControllingWithParent()
+                    Column(modifier = Modifier.fillMaxSize().padding(innerPadding), horizontalAlignment = Alignment.CenterHorizontally) {
+                        NavHost(
+                            navController = navController,
+                            startDestination = NavigationItem.HomeScreen.route
+                        ) {
+                            composable(NavigationItem.HomeScreen.route) {
+                                HomeScreen {
+                                    navController.navigate(it.route)
+                                }
+                            }
+                            composable(NavigationItem.NestedViewPager.route) {
+                                NestingOfViewPagerAndControllingWithParent()
+                            }
+                            composable(NavigationItem.DynamicViewPager.route) {
+                                DynamicallyAddRemovePagesInViewPager()
+                            }
+                            composable(NavigationItem.CarouselViewPager.route) {
+                                CarouselImplementationWithTabIndicators()
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
+
